c0569fa316fd1a565f4c0a600727fc7e4dd6911e
Illia Zviezdin <82528000+KetoAcc2@users.noreply.github.com>
2021-05-03 21:30:34 +0200

    update commit

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
+from hashlib import sha256
+
 import uvicorn
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
-from fastapi import Request
-from datetime import datetime
+from fastapi import FastAPI, Response, status
+from pydantic import BaseModel
 
 app = FastAPI()
 
+app.secret_key = "very constant and random secret, best 64+ characters, I love elephants and bananas"
+app.access_tokens = []
 
-@app.get("/hello", response_class=HTMLResponse)
-def read_main():
-    today_date = datetime.now()
-    today_date = today_date.strftime("%Y-%m-%d")
-    return "<h1>" \
-           "Hello! Today date is " + today_date + \
-           "</h1>"
+
+class Data(BaseModel):
+    username: str
+    password: str
+
+
+@app.post("/login_session")
+def login_session(data: Data, response: Response):
+    response.status_code = status.HTTP_201_CREATED
+
+    if data.username is None or data.username != '4dm1n' or\
+            data.password is None or data.password != 'NotSoSecurePa$$':
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return response
+
+    session_token = sha256(f"{data.username}{data.password}{app.secret_key}".encode()).hexdigest()
+    app.access_tokens.append(session_token)
+    response.set_cookie(key="session_token", value=session_token)
+
+    return response
+
+
+@app.post("/login_token")
+def login_session(data: Data, response: Response):
+    response.status_code = status.HTTP_201_CREATED
+
+    if data.username is None or data.username != '4dm1n' or\
+            data.password is None or data.password != 'NotSoSecurePa$$':
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return response
+
+    session_token = sha256(f"{data.username}{data.password}{app.secret_key}".encode()).hexdigest()
+    app.access_tokens.append(session_token)
+    response.set_cookie(key="session_token", value=session_token)
+
+    return {"token": session_token}
 
 
 if __name__ == "__main__":
