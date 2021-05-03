7d3013a2eecc6e56c71d1d9c6e90c13b5ff0f710
Illia Zviezdin <82528000+KetoAcc2@users.noreply.github.com>
2021-05-03 22:13:49 +0200

    update commit

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from hashlib import sha256
 
 import uvicorn
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Response, status, Request
 from pydantic import BaseModel
 
 app = FastAPI()
 
 app.secret_key = "very constant and random secret, best 64+ characters, I love elephants and bananas"
 app.access_tokens = []
+app.session_token = str()
 
 
 class Data(BaseModel):
@@ -26,13 +27,14 @@ def login_session(data: Data, response: Response):
 
     session_token = sha256(f"{data.login}{data.password}{app.secret_key}".encode()).hexdigest()
     app.access_tokens.append(session_token)
+    app.session_token = session_token
     response.set_cookie(key="session_token", value=session_token)
 
     return response
 
 
 @app.post("/login_token")
-def login_session(data: Data, response: Response):
+def login_session(data: Data, response: Response, request: Request):
     response.status_code = status.HTTP_201_CREATED
 
     if data.login is None or data.login != '4dm1n' or\
@@ -40,11 +42,12 @@ def login_session(data: Data, response: Response):
         response.status_code = status.HTTP_401_UNAUTHORIZED
         return response
 
-    session_token = sha256(f"{data.login}{data.password}{app.secret_key}".encode()).hexdigest()
-    app.access_tokens.append(session_token)
-    response.set_cookie(key="session_token", value=session_token)
+    session_token = request.cookies['session_token']
+    if session_token == app.session_token:
+        return {"token": session_token}
 
-    return {"token": session_token}
+    response.status_code = status.HTTP_401_UNAUTHORIZED
+    return response
 
 
 if __name__ == "__main__":
