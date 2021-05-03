10c7ca21c436f6b06e0cf076692948803c846fbb
Illia Zviezdin <82528000+KetoAcc2@users.noreply.github.com>
2021-05-03 23:07:45 +0200

    update commit

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import secrets
 from hashlib import sha256
 
 import uvicorn
-from fastapi import FastAPI, Response, status, Request
+from fastapi import FastAPI, Response, status, Request, Depends
 from pydantic import BaseModel
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
 
@@ -10,22 +12,21 @@ app.secret_key = "very constant and random secret, best 64+ characters, I love e
 app.access_tokens = []
 app.session_token = str()
 
-
-class Data(BaseModel):
-    login: str
-    haslo: str
+security = HTTPBasic()
 
 
 @app.post("/login_session")
-def login_session(*, data: Data, response: Response):
+def login_session(response: Response, credentials: HTTPBasicCredentials = Depends(security)):
     response.status_code = status.HTTP_201_CREATED
 
-    if data.login is None or data.login != '4dm1n' or\
-            data.haslo is None or data.haslo != 'NotSoSecurePa$$':
+    correct_username = secrets.compare_digest(credentials.username, "4dm1n")
+    correct_password = secrets.compare_digest(credentials.password, "NotSoSecurePa$$")
+
+    if not (correct_username and correct_password):
         response.status_code = status.HTTP_401_UNAUTHORIZED
         return response
 
-    session_token = sha256(f"{data.login}{data.haslo}{app.secret_key}".encode()).hexdigest()
+    session_token = sha256(f"{correct_username}{correct_password}{app.secret_key}".encode()).hexdigest()
     app.access_tokens.append(session_token)
     app.session_token = session_token
     response.set_cookie(key="session_token", value=session_token)
@@ -34,16 +35,17 @@ def login_session(*, data: Data, response: Response):
 
 
 @app.post("/login_token")
-def login_session(*, data: Data, response: Response, request: Request):
-    response.status_code = status.HTTP_201_CREATED
+def login_session(response: Response, request: Request, credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "4dm1n")
+    correct_password = secrets.compare_digest(credentials.password, "NotSoSecurePa$$")
 
-    if data.login is None or data.login != '4dm1n' or\
-            data.haslo is None or data.haslo != 'NotSoSecurePa$$':
+    if not (correct_username and correct_password):
         response.status_code = status.HTTP_401_UNAUTHORIZED
         return response
 
     session_token = request.cookies['session_token']
     if session_token == app.session_token:
+        response.status_code = status.HTTP_201_CREATED
         return {"token": session_token}
 
     response.status_code = status.HTTP_401_UNAUTHORIZED
